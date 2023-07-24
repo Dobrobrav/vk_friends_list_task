733c4235c31d5490b0c7c151948592fe332958c1
Dobrobrav <kalyakulin.semen@gmail.com>
2023-07-24 17:34:05 +0700

    refactoring: shrinking to just 5 .py files

--- input_args_loaders.py
+++ input_args_loaders.py
@@ -1,18 +1,6 @@
 import argparse
 from abc import ABC, abstractmethod
-from typing import NamedTuple, Literal
-from . import common
-import logs.utils.for_input_args_loaders
-from .exceptions import InvalidInputError
-
-
-class InputArgs(NamedTuple):
-    auth_token: str
-    user_id: int
-    output_format: Literal['csv', 'tsv', 'json']
-    output_path: str
-    page: int | None
-    limit: int | None
+import common
 
 
 class IInputArgsLoader(ABC):
@@ -27,8 +15,8 @@ class IInputArgsLoader(ABC):
 class TerminalArgsLoader(IInputArgsLoader):
     """ Loads input arguments from terminal """
 
-    def load(self) -> InputArgs:
-        logs.utils.for_input_args_loaders.log_start_loading_terminal_args()
+    def load(self) -> common.InputArgs:
+        log_start_loading_terminal_args()
 
         parser = argparse.ArgumentParser()
         parser.add_argument(
@@ -58,14 +46,14 @@ class TerminalArgsLoader(IInputArgsLoader):
         self._validate_input_args(args)
         filtered_args = self._filter_input_args(args)
 
-        logs.utils.for_input_args_loaders.log_finish_loading_terminal_args()
+        log_finish_loading_terminal_args()
 
         return filtered_args
 
     def _filter_input_args(self,
                            input_args: argparse.Namespace,
-                           ) -> InputArgs:
-        return InputArgs(
+                           ) -> common.InputArgs:
+        return common.InputArgs(
             auth_token=input_args.auth_token,
             user_id=input_args.user_id,
             output_path=input_args.out_path,
@@ -77,12 +65,12 @@ class TerminalArgsLoader(IInputArgsLoader):
     @staticmethod
     def _validate_input_args(input_args: argparse.Namespace,
                              ) -> None:
-        logs.utils.for_input_args_loaders.log_start_validating_terminal_args()
+        log_start_validating_terminal_args()
 
         try:
             common.validate_positive_int_or_none(input_args.page)
         except (TypeError, ValueError):
-            raise InvalidInputError(
+            raise common.InvalidInputError(
                 arg_name='page',
                 expected_value_descr='a positive integer',
                 log_error_descr='Invalid page value',
@@ -90,10 +78,27 @@ class TerminalArgsLoader(IInputArgsLoader):
         try:
             common.validate_positive_int_or_none(input_args.limit)
         except (TypeError, ValueError):
-            raise InvalidInputError(
+            raise common.InvalidInputError(
                 arg_name='limit',
                 expected_value_descr='a positive integer',
                 log_error_descr='Invalid limit value',
             )
 
-        logs.utils.for_input_args_loaders.log_finish_validating_terminal_args()
+        log_finish_validating_terminal_args()
+
+
+# LOGGING FUNCTIONS
+def log_start_loading_terminal_args():
+    common.logger.info(f"Start loading args from terminal")
+
+
+def log_finish_loading_terminal_args():
+    common.logger.info(f"Successfully loaded args from terminal")
+
+
+def log_start_validating_terminal_args():
+    common.logger.info(f"Start validating args from terminal")
+
+
+def log_finish_validating_terminal_args():
+    common.logger.info(f"Successfully validated args from terminal")
